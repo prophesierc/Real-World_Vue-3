c6e4df375b20ee9a2aa2f91d82a4439083b28aa8
ProphesierC <bwknowles98@gmail.com>
2025-01-18 23:29:31 -0500

    update 6 files and delete 5 files

---
+++ Real-World_Vue-3/src/components/EventCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="event-card">
+        <span>@ {{ event.time }} on {{ event.date }} </span>
+        <h4>{{ event.title }}</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'EventCard',
+        props: {
+            event: Object, 
+        }
+    }
+</script>
+
+<style scoped>
+    .event-card {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+        width: 100%;
+        height: 100%;
+        cursor: pointer; 
+        border: lpx solid #39495c;
+        margin-bottom: 18px;
+    }
+    .event-card:hover {
+        transform: scale(1.01);
+        box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+    }
+</style>
